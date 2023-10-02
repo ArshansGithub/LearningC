73f6a569679fcbf0e5e2b65647168e4fc73b33c4
Arshan <arshan@arshan.dev>
2023-10-01 19:26:41 -0700

    problem2 baby

---
+++ HSPC Problems/Problem2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello world");
+
+    int sample[] = {4, 1, 2, -3, 4};
+
+    int combo;
+
+    for (int outer = 0; outer < sample[0]; outer++){
+        if (outer == sample[0]) {
+            continue;
+        }
+        for (int inner = 0; inner < sample[0]; inner++) {
+            
+        }
+    }
+
+    return 0;
+}
