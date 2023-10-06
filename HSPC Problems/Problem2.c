eb74c6371981a85aa0aa1addfb31dd88ecfc66c3
Arshan <arshan@arshan.dev>
2023-10-06 13:20:57 -0700

    completed Problem2

--- HSPC Problems/Problem2.c
+++ HSPC Problems/Problem2.c
@@ -1,20 +1,70 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello world");
+    int arrlen;
+    int* daArray;
 
-    int sample[] = {4, 1, 2, -3, 4};
+    printf("Length?\n> "); 
+    scanf("%d", &arrlen);
+    
+    daArray = (int*) malloc(arrlen * sizeof(int));
 
-    int combo;
+    if (daArray == NULL) {
+        printf("Couldn't assign memory, sorry lol");
+        return 0;
+    }
 
-    for (int outer = 0; outer < sample[0]; outer++){
-        if (outer == sample[0]) {
+    int i = 0;
+
+    int done = 0;
+
+    printf("\nInput ?\nSay done when finished"); 
+
+    do
+    {
+
+        int arr;
+
+        /*for (arr = 0; arr < arrlen; arr++) {
+            printf("%d | ", arr);
+            printf("Arr: %d\n", daArray[arr]);
+        }*/
+
+        char userInput[] = "";
+
+        printf("\n\n> "); 
+        scanf("%s", userInput); 
+
+        if (strstr(userInput, "done") != NULL) {
+            done = 1;
+        }
+        else {
+            daArray[i] = atoi(userInput);
+            i++;
+            //printf("%d", atoi(userInput));
+        }
+    } while (done == 0);
+
+
+    
+    int combo = 0;
+
+    for (int outer = 0; outer < arrlen; outer++){
+        //printf("got here");
+        //printf("%d\n%d\n", arrlen, daArray[outer]);
+        if (daArray[outer] <= 0) {
+            //printf("Looping");
             continue;
         }
-        for (int inner = 0; inner < sample[0]; inner++) {
-            
-        }
+        //printf("Adding %d with %d\n", combo, daArray[outer]);
+        combo += daArray[outer];
+
     }
+
+    printf("Result: %d", combo);
 
     return 0;
 }
+
